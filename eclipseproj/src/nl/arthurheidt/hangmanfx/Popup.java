02b5fe91f8b6e96786dcd63c70701662fe7229b4
Arthur Heidt <arthur@arthurheidt.nl>
2018-03-01 18:55:34 +0100

    RELEASE 0.2

---
+++ eclipseproj/src/nl/arthurheidt/hangmanfx/Popup.java
@@ -0,0 +1,82 @@
+package nl.arthurheidt.hangmanfx;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public abstract class Popup {
+	public static void showAlert(String title, String text, String buttonText) {
+		Stage popup = new Stage();
+		popup.initModality(Modality.APPLICATION_MODAL);
+		popup.setTitle(title);
+		popup.setAlwaysOnTop(true);
+
+		Label l = new Label(text);
+		Button b = new Button(buttonText);
+		b.setOnAction(e -> {
+			popup.close();
+			return;
+		});
+
+		VBox vb = new VBox(10);
+		vb.setAlignment(Pos.CENTER);
+		vb.getChildren().addAll(l, b);
+
+		Scene s = new Scene(vb, 300, 150);
+		popup.setScene(s);
+		popup.showAndWait();
+
+	}
+
+	public static String getString(String title, String text, boolean validate) {
+		Stage popup = new Stage();
+		popup.initModality(Modality.APPLICATION_MODAL);
+		popup.setTitle(title);
+		popup.setAlwaysOnTop(true);
+
+		Label l = new Label(text);
+		TextField t = new TextField();
+		Button b = new Button("OK");
+		Label le = new Label("Invalid input!");
+		le.setVisible(false);
+		
+		t.setOnAction(e -> {
+			validateAndExit(popup, t, validate, le);});
+		b.setOnAction(e -> {
+			validateAndExit(popup, t, validate, le);
+		});
+
+		VBox vb = new VBox(10);
+		vb.setAlignment(Pos.CENTER);
+		vb.getChildren().addAll(l, t, b, le);
+
+		Scene s = new Scene(vb, 300, 150);
+		popup.setScene(s);
+		popup.showAndWait();
+		return t.getText();
+
+	}
+
+	private static void validateAndExit(Stage popup, TextField t, boolean validate, Label le) {
+		if (!validate) {
+			popup.close();
+		} else {
+			if (t.getText().isEmpty() || t.getText().contains(" ")) {
+				le.setVisible(true);
+			} else {
+				popup.close();
+			}
+		}
+	}
+
+	public static void showAndQuit(String title, String text, String buttonText, int exitCode) {
+		showAlert(title, text, buttonText);
+		System.exit(exitCode);
+	}
+
+}
--- eclipseproj/src/nl/arthurheidt/hangmanfx/UIController.java
+++ eclipseproj/src/nl/arthurheidt/hangmanfx/UIController.java
@@ -18,84 +18,123 @@ import javafx.scene.shape.Shape;
 
 /**
  * Controller class for UI.fxml
+ * 
  * @author Arthur Heidt
  *
  */
 public class UIController implements Initializable {
-    
-    public final static String WORD = "vrachtwagenventiel";
-    private String guessWord;
-    private char[] guessWordArr;
-    private int maxTries = 11;
-    private int currentTries = 0;
-    
-    @FXML
-    private SplitPane rootPane;
-    @FXML
-    private HBox alphabet, theWord;
-    @FXML
-    private Arc mouth;
-    @FXML
-    private Shape hang1, hang2, hang3, hang4, hang5, hang6, hang7, hang8, hang9, hang10, hang11;
-    @FXML
-    private AnchorPane face, man;
-    private ArrayList<Node> manNodes;
-    
-    
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-	
-	//Generate the guess buttons
-	for (char c = 'A'; c <= 'Z'; c++) {
-	    String content = "" + c;
-	    Button b = new Button(content);
-	    b.setOnAction( e -> handleAlphabetButton(e));
-	    alphabet.getChildren().add(b);
-	}
-	
-	//Generate the word fields
-	guessWord = WORD.toUpperCase();
-	guessWordArr = guessWord.toCharArray();
-	for (int i = 0; i < guessWordArr.length; i++) {
-	    Label l = new Label(("" + guessWordArr[i]));
-	    l.setVisible(false);
-	    theWord.getChildren().add(l);
-	}
-	
-	//Generate list
-	manNodes = new ArrayList<Node>(manNodes.getChildren);
-	
-	System.out.println("Game started");
-	
-    }
-    
-    private void handleAlphabetButton(ActionEvent e) {
-	Button source = (Button) e.getSource();
-	source.setDisable(true);
-	if (!checkLetter(source.getText())) {
-	    drawNextPart();
-	}
-    }
-    
-    private boolean checkLetter(String letter) {
-	boolean succes = false;
-	for (Node n : theWord.getChildren()) {
-	    if (n instanceof Label) {
-		Label l = (Label) n;
-		if (l.getText().equals(letter)) {
-		    l.setVisible(true);
-		    succes = true;
+
+	private String word;
+	private String guessWord;
+	private char[] guessWordArr;
+	private int maxTries = 11;
+	private int currentTries = 0;
+
+	@FXML
+	private SplitPane rootPane;
+	@FXML
+	private HBox alphabet, theWord;
+	@FXML
+	private Arc mouth;
+	@FXML
+	private Shape hang1, hang2, hang3, hang4, hang5, hang6, hang7, hang8, hang9, hang10, hang11;
+	@FXML
+	private AnchorPane face, man;
+	private ArrayList<Node> manNodes;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		//Get the word
+		word = Popup.getString("Welcome", "Please enter a word for your opponent...", true).toUpperCase();
+		
+		// Generate the guess buttons
+		for (char c = 'A'; c <= 'Z'; c++) {
+			String content = "" + c;
+			Button b = new Button(content);
+			b.setOnAction(e -> handleAlphabetButton(e));
+			alphabet.getChildren().add(b);
 		}
-	    }
+
+		// Generate the word fields
+		guessWord = word.toUpperCase();
+		guessWordArr = guessWord.toCharArray();
+		for (int i = 0; i < guessWordArr.length; i++) {
+			Label l = new Label(("" + guessWordArr[i]));
+			l.setVisible(false);
+			theWord.getChildren().add(l);
+		}
+
+		// Generate list
+		manNodes = new ArrayList<Node>(man.getChildren());
+
+		System.out.println("Game started");
+
 	}
-	return succes;
-    }
-    
-    private void drawNextPart() {
-	Node n = hangNodes.get
-	
-	currentTries++;
-	
-    }
+
+	private void handleAlphabetButton(ActionEvent e) {
+		Button source = (Button) e.getSource();
+		source.setDisable(true);
+		boolean wasInWord = parseLetter(source.getText());
+		System.out.println(wasInWord);
+		if (!wasInWord) {
+			drawNextPart();
+		}
+		checkWinLose();
+	}
+
+	private void checkWinLose() {
+		if (currentTries >= maxTries) {
+			mouth.setStartAngle(0);
+			mouth.setLayoutY(25);
+			
+			face.setVisible(true);
+			for (Node n : alphabet.getChildren()) {
+				n.setDisable(true);
+			}
+			Popup.showAndQuit("TOO BAD", "Seems like you've lost...", "Exit", 0);
+		} else {
+			boolean win = true;
+			for (Node n : theWord.getChildren()) {
+				if (!n.isVisible()) {
+					win = false;
+				}
+			}
+			if (win) {
+				for (int i = 5; i < manNodes.size(); i++) {
+					manNodes.get(i).setVisible(true);
+				}
+				for (int i = 0; i < 5; i++) {
+					manNodes.get(i).setVisible(false);
+				}
+				face.setVisible(true);
+				for (Node n : alphabet.getChildren()) {
+					n.setDisable(true);
+				}
+				Popup.showAndQuit("SUCCES", "You have won the game.", "Exit", 0);
+			}
+		}
+		
+	}
+
+	private boolean parseLetter(String letter) {
+		boolean retValue = false;
+		for (Node n : theWord.getChildren()) {
+			if (n instanceof Label) {
+				Label l = (Label) n;
+				if (l.getText().equals(letter)) {
+					retValue = true;
+					l.setVisible(true);
+				}
+			}
+		}
+		return retValue;
+	}
+
+	private void drawNextPart() {
+		if (currentTries < maxTries) {
+			manNodes.get(currentTries).setVisible(true);
+			currentTries++;
+		}
+	}
 
 }
